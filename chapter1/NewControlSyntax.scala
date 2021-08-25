2d9060fadf19168f66f6989e35c6f5a1ae93ecdb
taretmch <tomochuri25@gmail.com>
2021-08-26 01:50:32 +0900

    Add NewControlSyntax.scala

---
+++ chapter1/NewControlSyntax.scala
@@ -0,0 +1,33 @@
+import java.io.IOException
+
+object NewControlSyntax:
+  def ifFunc1(x: Int) =
+    if x < 0 then
+      "negative"
+    else if x == 0 then
+      "zero"
+    else
+      "positive"
+
+  def ifFunc2(x: Int) =
+    if x < 0 then -x else x
+
+  // Needs scala.runtime.java8
+  //
+  //def whileFunc1(x: Int, f: Int => Int) =
+  //  var tmp = x
+  //  while tmp >= 0 do tmp = f(tmp)
+
+  def forFunc1(xs: Seq[Int]) =
+    for x <- xs if x > 0
+    yield x * x
+
+  def forFunc2(xs: Seq[Int], ys: Seq[Int]) =
+    for
+      x <- xs
+      y <- ys
+    do
+      println(x + y)
+
+  def tryFunc1 =
+    try throw new IOException catch case ex: IOException => println("error")
