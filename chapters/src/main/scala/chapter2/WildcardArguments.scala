18acd17f56c7a7e594da5a9a84b0527d8e8ec99e
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2021-09-17 03:42:00 +0900

    Fix wildcard argument of type

--- chapters/src/main/scala/chapter2/WildcardArguments.scala
+++ chapters/src/main/scala/chapter2/WildcardArguments.scala
@@ -3,7 +3,7 @@ package chapter2
 object WildcardArguments:
 
   def f1: Int => Int = _ + 3
-  def f2(list: List[_]): Option[_] =
+  def f2(list: List[?]): Option[?] =
     list.headOption
   
   trait Functor[F[_]] {
