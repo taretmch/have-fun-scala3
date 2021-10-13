15495fd9737b3ef194f38474b9e75f4c63f00208
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2021-10-14 02:39:40 +0900

    Fix compile error of exercise test code

--- exercise/src/main/scala/exercise/chapter3/Question2.scala
+++ exercise/src/main/scala/exercise/chapter3/Question2.scala
@@ -2,4 +2,6 @@ package exercise.chapter3
 
 /** Question: Using Ord */
 object Question2:
+
+  def max[T](list: List[T]): T = ???
 end Question2
--- exercise/src/main/scala/exercise/chapter3/Question3.scala
+++ exercise/src/main/scala/exercise/chapter3/Question3.scala
@@ -2,4 +2,12 @@ package exercise.chapter3
 
 /** Question: ExtensionMethods */
 object Question3:
+
+  extension [T](x: T)
+    def < (y: T): Boolean = ???
+    def > (y: T): Boolean = ???
+
+  extension [T](list: List[T])
+    def max: T = ???
+
 end Question3
