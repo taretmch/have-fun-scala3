ac5fde3a278d10c7388f649d8a986ea4b223627e
taretmch <tomoki.mizogami@gmail.com>
2023-08-14 21:41:49 +0900

    Add extension method for Content

--- basic/src/main/scala/have/fun/scala3/basic/types/union/ContentAsUnion.scala
+++ basic/src/main/scala/have/fun/scala3/basic/types/union/ContentAsUnion.scala
@@ -2,6 +2,15 @@ package have.fun.scala3.basic.types.union
 
 type ContentAsUnion = TextContent | ImageContent
 
-case class TextContent(text: String)
+// To use this extension method, import this
+extension (content: ContentAsUnion)
+  def contentType: ContentType =
+    content match
+      case TextContent(_) => ContentType.Text
+      case ImageContent(_, _) => ContentType.Image
 
-case class ImageContent(url: String, alt: Option[String])
+case class TextContent(text: String):
+  def contentType: ContentType = ContentType.Text
+
+case class ImageContent(url: String, alt: Option[String]):
+  def contentType: ContentType = ContentType.Image
